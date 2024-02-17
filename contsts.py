639895b859bbc67bf258adfe320d524155137a41
koalmsk <denkosarev@gmail.com>
2024-02-18 01:53:47 +0300

    no ready task
    https://lms.yandex.ru/courses/1054/groups/8671/lessons/5971/tasks/44999

---
+++ contsts.py
@@ -0,0 +1,25 @@
+STAFF = "инженер", "строитель"
+
+
+PROFI_LIST_CONST = [
+    "инженер",
+    "строитель", 
+    "и бла", 
+    "бла",
+    "бла", 
+    "бла", 
+    "бла"
+]
+
+
+PROFILE_DICT = {
+
+    "title": "", 
+    "surname": "",
+    "name": "",
+    "education": "",
+    "profession": "",
+    "sex": "",
+    "motivation": "",
+    "ready": "",
+}
--- main.py
+++ main.py
@@ -1,17 +1,6 @@
 from flask import Flask, render_template
-import flask
+import contsts
 
-
-STAFF = "инженер", "строитель"
-PROFI_LIST_CONST = [
-    "инженер",
-    "строитель", 
-    "и бла", 
-    "бла",
-    "бла", 
-    "бла", 
-    "бла"
-]
 
 app = Flask(__name__)
 
@@ -22,7 +11,7 @@ def index(input_title):
 
 @app.route("/training/<prof_input>")
 def profi(prof_input):
-    to_page = ("Инженерные тренажеры" if prof_input in STAFF else "Научные симуляторы")
+    to_page = ("Инженерные тренажеры" if prof_input in contsts.STAFF else "Научные симуляторы")
     return render_template("training.html", prof=to_page)
 
 
@@ -30,7 +19,12 @@ def profi(prof_input):
 def profi_list(prof_input):
     if prof_input not in ["ol", "ul"]:
         return render_template("error.html")
-    return render_template("profi.html", mode=prof_input, prof=PROFI_LIST_CONST)
+    return render_template("profi.html", mode=prof_input, prof=contsts.PROFI_LIST_CONST)
+
+@app.route("/answer")
+@app.route("/auto_answer")
+def auto_answer():
+    pass
 
 
 if __name__ == "__main__":
