c14248049305acce43dab28780884c268a599a6b
koalmsk <denkosarev@gmail.com>
2024-02-18 15:54:59 +0300

    фотки поправил

--- main.py
+++ main.py
@@ -9,8 +9,11 @@ app = Flask(__name__)
 
 @app.route("/training/<prof_input>")
 def index(prof_input):
-    to_page = ("Инженерные тренажеры" if prof_input in STAFF else "Научные симуляторы")
-    return render_template("index.html", prof=to_page)
+    to_page = (
+        ("Инженерные тренажеры", "/static/imges/it.jpeg") if prof_input.lower() in STAFF 
+        else ("Научные симуляторы", "/static/imges/ns.jpeg")
+        )
+    return render_template("index.html", prof=to_page[0], photo=to_page[1])
 
 
 if __name__ == "__main__":
